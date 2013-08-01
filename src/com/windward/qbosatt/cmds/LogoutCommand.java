7d8cea76ceaff5fdca456d3c3d8a4d67f4c713df
Andrew Glover <ajglover@gmail.com>
2013-08-01 18:32:05 -0400

    added tests w/powermock for logout

---
+++ src/com/windward/qbosatt/cmds/LogoutCommand.java
@@ -0,0 +1,38 @@
+package com.windward.qbosatt.cmds;
+
+import com.qbos.QTP.InvalidCredentialsException;
+import com.qbos.QTP.QTP;
+import com.realops.common.enumeration.Status;
+import com.realops.common.xml.XML;
+import com.realops.foundation.adapterframework.AdapterRequest;
+import com.realops.foundation.adapterframework.AdapterResponse;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: aglover
+ * Date: 8/1/13
+ * Time: 5:01 PM
+ */
+public class LogoutCommand extends AbstractCommand {
+    @Override
+    public AdapterResponse execute(AdapterRequest adapterRequest) {
+        try {
+            XML logoutXML = adapterRequest.getData();
+            QTP instance = QTP.Create(logoutXML.getChild("qsi").getText(), logoutXML.getChild("ticket").getText());
+            instance.logOut();
+            return getSuccessResponse();
+        } catch (Exception e) {
+            if (e instanceof InvalidCredentialsException) {
+                return getSuccessResponse();
+            } else {
+                return new AdapterResponse(300, "FAILURE: " + e.getLocalizedMessage(),
+                        new XML("response").setText("FAILURE"), Status.ERROR);
+            }
+        }
+    }
+
+    private AdapterResponse getSuccessResponse() {
+        return new AdapterResponse(300, "QTP Logout successful",
+                new XML("response").setText("successful logout"), Status.SUCCESS);
+    }
+}
---
+++ test/test/com/windward/qbosatt/LogoutCommandTest.java
@@ -0,0 +1,42 @@
+package test.com.windward.qbosatt;
+
+import com.qbos.QTP.QTP;
+import com.realops.common.xml.XML;
+import com.realops.foundation.adapterframework.AdapterRequest;
+import com.realops.foundation.adapterframework.AdapterResponse;
+import com.windward.qbosatt.QbosAdapter;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: aglover
+ * Date: 8/1/13
+ * Time: 5:02 PM
+ */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(QTP.class)
+public class LogoutCommandTest {
+
+    @Test
+    public void testLoginRequest() throws Exception {
+        PowerMockito.mockStatic(QTP.class);
+        QTP qtpThing = mock(QTP.class);
+        when(QTP.Create("dm2q", "0C4F7501U1143U5955UDC8C1EB43B06C988")).thenReturn(qtpThing);
+        XML xml = XML.read("etc/test-logout-req.xml");
+        AdapterRequest request = new AdapterRequest(xml);
+        QbosAdapter adaptor = new QbosAdapter();
+        adaptor.setQtpInstance(qtpThing);
+        AdapterResponse adapterResponse = adaptor.performAction(request);
+        assertNotNull("adapterResponse was not null?", adapterResponse);
+        verify(qtpThing, times(1)).logOut();
+        assertEquals("successful logout", adapterResponse.getData().getText());
+    }
+}
