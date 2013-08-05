326a25992fa9aa4600784f7544793b214a383df9
Andrew Glover <ajglover@gmail.com>
2013-08-04 20:25:41 -0400

    added update + read

---
+++ test/test/com/windward/qbosatt/ReadCommandTest.java
@@ -0,0 +1,47 @@
+package test.com.windward.qbosatt;
+
+import com.qbos.QTP.Applet;
+import com.qbos.QTP.QTP;
+import com.realops.common.xml.XML;
+import com.realops.foundation.adapterframework.AdapterRequest;
+import com.realops.foundation.adapterframework.AdapterResponse;
+import com.windward.qbosatt.QbosAdapter;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: aglover
+ * Date: 8/4/13
+ * Time: 8:21 PM
+ */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(QTP.class)
+public class ReadCommandTest {
+    @Test
+    public void testUpdateCommand() throws Exception {
+        PowerMockito.mockStatic(QTP.class);
+        QTP qtpThing = mock(QTP.class);
+        when(QTP.Create("dm2q", "0C4F7501U1143U5955UDC8C1EB43B06C988")).thenReturn(qtpThing);
+        when(qtpThing.updateRecord(any(Applet.class))).thenReturn(new Long(10000000000L));
+        XML xml = XML.read("etc/test-read-req.xml");
+        AdapterRequest request = new AdapterRequest(xml);
+        QbosAdapter adaptor = new QbosAdapter();
+        adaptor.setQtpInstance(qtpThing);
+        AdapterResponse adapterResponse = adaptor.performAction(request);
+        PowerMockito.verifyStatic(Mockito.times(1));
+        QTP.Create("dm2q", "0C4F7501U1143U5955UDC8C1EB43B06C988");
+        assertNotNull("adapterResponse was not null?", adapterResponse);
+        verify(qtpThing, times(1)).updateRecord(any(Applet.class));
+        assertEquals("10000000000", adapterResponse.getData().getText());
+    }
+}
